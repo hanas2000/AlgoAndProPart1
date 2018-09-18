dc04cd946da496d18ddb0a70ac73979a9fb58e15
hanas2000 <hanas2000@i.ua>
2018-09-18 02:52:14 -0400

    add 2 files

---
+++ Lab2/lab2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <locale.h>
+#include <stdlib.h>
+
+int main() {
+int a=0;
+int b=0;
+
+    for(;a<100;a+=2)
+    {
+        b += a;
+    }
+    
+    printf("%d\n",b);
+    
+  return 0;
+}
